862b89da80290b9805066fa9c692a95503c97de3
Jinghao Shi <jinghaos@buffalo.edu>
2014-02-28 11:20:48 -0500

    add client update_rf

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
-import time
+import time, json
+
+import utils
 
 class Client(object) :
 
@@ -11,22 +13,37 @@ class Client(object) :
     self.rx_bytes = None
     self.tx_bytes = None
     self.signal = None
+
+    self.frequency = None
+    self.neighbors = None
+    self.channel_load = None
+
     self.last_updated = None
 
-  @classmethod
-  def create(cls, mac, info) :
-    client = Client()
-    client.mac = mac
-    client.rx_bytes = int(info['rx_bytes'])
-    client.tx_bytes = int(info['tx_bytes'])
-    client.signal = int(info['signal'])
-    client.last_updated = time.time()
-
-    return client
-    
-
   def update_info(self, info) :
-    pass
+    self.rx_bytes = int(info['rx_bytes'])
+    self.tx_bytes = int(info['tx_bytes'])
+    self.signal = int(info['signal'])
+    self.last_updated = time.time()
 
-  def update_rf(msg) :
-    pass
+  def update_rf(self, msg) :
+    utils.log("Updating RF condition for " + self.mac)
+
+    self.frequency = int(msg['frequency'])
+    self.neighbors = dict()
+    self.channel_load = dict()
+
+    for info in msg['traffics'] :
+      utils.log(str(info))
+
+      srcMac = info['srcMac']
+      signal = int(info['signal'])
+      channel = int(info['channel'])
+      bytes = int(info['bytes'])
+
+      if srcMac not in self.neighbors or signal > self.neighbors[srcMac] :
+        self.neighbors[srcMac] = signal
+
+      self.channel_load[channel] = self.channel_load.get(channel, 0) + bytes
+
+    self.last_updated = time.time()
