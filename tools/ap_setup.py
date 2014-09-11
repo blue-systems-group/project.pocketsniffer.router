ac66af2541c7e34ec49bb9426264813fe33b86a3
Jinghao Shi <jinghaos@buffalo.edu>
2014-09-11 11:01:53 -0400

    refactor

--- tools/ap_setup.py
+++ tools/ap_setup.py
@@ -3,19 +3,19 @@
 import os
 import sys
 import tempfile
+import shutil
+import subprocess
 from datetime import datetime as dt
 import pexpect
 import argparse
 import glob
-import pxssh
-import getpass
 
-DEFAULT_KEY = '~/.ssh/id_isa.pub'
+DEFAULT_KEY = '~/.ssh/id_rsa.pub'
 DEFAULT_SSID = "PocketSniffer"
 DEFAULT_ROOT_PASSWORD = 'BM5jJBAiUaHCNFdJDUzJxdOuxi5CiLs9'
 DEFAULT_AP_PASSWORD =   'LDR9OXnevs5lBlCjz0MNga2H40DlT2m0'
 DEFAULT_GATEWAY =     '192.168.1.1'
-DEFAULT_TEMPLATE_DIR = glob.glob('./templates/')[0]
+DEFAULT_TEMPLATE_DIR = './templates/'
 DEFAULT_USER_NAME = 'jinghaos'
 DEFAULT_USER_PASS = 'jinghaos'
 
@@ -27,24 +27,51 @@ SSH_ARGS = '-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 KEY_FILE_NAME = 'id_isa.pub'
 DEV_PATH = '/dev/sda1'
 
+USB_MODULES = ['kmod-usb-core', 'kmod-usb-ohci', 'kmod-usb-uhci', 'kmod-usb2',
+    'usbutils', 'kmod-usb-storage', 'kmod-fs-ext4', 'kmod-usb-storage-extra',
+    'block-mount', 'e2fsprogs']
+
+EXTRA_MODULES = ['shadow-useradd', 'shadow-groupadd', 'shadow-usermod', 'sudo',
+    'sed', 'python', 'vim', 'bash', 'git']
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--hostname', required=True, help="Hostname")
 
-parser.add_argument('--key', required=False, default=DEFAULT_KEY, help="Public key file for ssh.")
-parser.add_argument('--ssid', required=False, default=DEFAULT_SSID, help="AP SSID")
-parser.add_argument('--rootpass', required=False, default=DEFAULT_ROOT_PASSWORD, help="Root password")
-parser.add_argument('--appass', required=False, default=DEFAULT_AP_PASSWORD, help="AP password")
-parser.add_argument('--gateway', required=False, default=DEFAULT_GATEWAY, help="AP gateway")
-parser.add_argument('--template', required=Fasle, default=DEFAULT_TEMPLATE_DIR, help="Configuration templates")
-parser.add_argument('--user', required=False, default=DEFAULT_USER_NAME, help="Non-privileged user to create.")
-parser.add_argument('--userpass', required=False, default=DEFAULT_USER_PASS, help="Password for non-privilged user")
-parser.add_argument('--nosudo', required=False, action='store_true', help="Do not add user to sudo group")
+parser.add_argument('--key', required=False, default=DEFAULT_KEY,
+    help="Public key file for ssh.")
+parser.add_argument('--ssid', required=False, default=DEFAULT_SSID,
+    help="AP SSID")
+parser.add_argument('--rootpass', required=False, default=DEFAULT_ROOT_PASSWORD,
+    help="Root password")
+parser.add_argument('--appass', required=False, default=DEFAULT_AP_PASSWORD,
+    help="AP password")
+parser.add_argument('--gateway', required=False, default=DEFAULT_GATEWAY,
+    help="AP gateway")
+parser.add_argument('--template', required=False, default=DEFAULT_TEMPLATE_DIR,
+    help="Configuration templates")
+parser.add_argument('--user', required=False, default=DEFAULT_USER_NAME,
+    help="Non-privileged user to create.")
+parser.add_argument('--userpass', required=False, default=DEFAULT_USER_PASS,
+    help="Password for non-privilged user")
+parser.add_argument('--nosudo', required=False, action='store_true',
+    help="Do not add user to sudo group")
 
 args = parser.parse_args()
 
 def log(s) :
   print >>sys.stdout, '[%s] %s' % (dt.now(), s)
+
+
+def install_packages(session, pkgs) :
+  session.sendline('opkg update')
+  match = session.expect(['Failed', PROMPT])
+  if match == 0 :
+    log("opkg update failed. Please check internet connection of router.")
+    exit(0)
+  else :
+    session.sendline('opkg install %s' % (' '.join(pkgs)))
+    session.expect(PROMPT)
 
 
 log("Checking root password...")
@@ -79,19 +106,21 @@ try :
     log("Already have password-free ssh access.");
   else :
     try :
-      key_file = glob.glob(args.key)[0]
+      key_file = glob.glob(os.path.expanduser(args.key))[0]
     except :
-      log("SSH public key file not found: %s." % (args.key))
+      log("SSH public key file not found: %s" % (args.key))
       exit(0)
 
     log("Copying key file %s ..." % (key_file))
-    child = pexpect.spawn('scp %s %s root@%s:/tmp/%s' % (SSH_ARGS, key_file, args.gateway, KEY_FILE_NAME))
+    child = pexpect.spawn('scp %s %s root@%s:/tmp/%s'
+        % (SSH_ARGS, key_file, args.gateway, KEY_FILE_NAME))
     child.expect('password')
     child.sendline(args.rootpass)
     child.expect(pexpect.EOF)
 
     log("Setting up password-free login ...")
-    child = pexpect.spawn('ssh %s root@%s \'cat /tmp/%s > /etc/dropbear/authorized_keys\'' % (SSH_ARGS, args.gateway, KEY_FILE_NAME))
+    child = pexpect.spawn('ssh %s root@%s \'cat /tmp/%s > /etc/dropbear/authorized_keys\''
+        % (SSH_ARGS, args.gateway, KEY_FILE_NAME))
     child.expect('password')
     child.sendline(args.rootpass)
     child.expect(pexpect.EOF)
@@ -102,7 +131,7 @@ except :
 
 
 try :
-  template_dir = glob.glob(args.template)[0]
+  template_dir = glob.glob(os.path.expanduser(args.template))[0]
 except :
   log("Template dir not found: %s." % (args.template))
   exit(0)
@@ -110,35 +139,32 @@ except :
 log("Making config files...")
 temp_dir = os.path.join(tempfile.mkdtemp(), 'templates')
 shutil.copytree(template_dir, temp_dir)
-try :
-  subprocess.check_call('sed -i -e \'s@%s@%s@g\' %s' % (HOSTNAME_PLACEHOLDER, args.hostname, os.path.join(temp_dir, "etc/config/system")))
-  subprocess.check_call('sed -i -e \'s@%s@%s@g\' %s' % (SSID_PLACEHOLDER, args.ssid, os.path.join(temp_dir, "etc/config/wireless")))
-  subprocess.check_call('sed -i -e \'s@%s@%s@g\' %s' % (PASSWORD_PLACEHOLDER, args.appass, os.path.join(temp_dir, "etc/config/wireless")))
-except :
-  log("Failed to make config file.")
-  exit(0)
+for placeholder, sub, f in zip([HOSTNAME_PLACEHOLDER, SSID_PLACEHOLDER, PASSWORD_PLACEHOLDER],
+    [args.hostname, args.ssid, args.appass], ['system', 'wireless', 'wireless']) :
+  cmd = 'sed -i -e \'s@%s@%s@g\' %s' % (placeholder, sub, os.path.join(temp_dir, "etc/config/%s" % (f)))
+  subprocess.check_call(cmd , shell=True)
 
 
 log("Overriding configurations files...")
-child = pexpect.spawn('scp %s -r %s/* root@%s:/' % (SSH_ARGS, temp_dir, args.gateway))
-child.expect(pexpect.EOF)
+subprocess.check_call('scp %s -r %s/* root@%s:/' % (SSH_ARGS, temp_dir, args.gateway), stdout=None, shell=True)
 
 
 log("Installing USB support...")
 child = pexpect.spawn('ssh %s root@%s' % (SSH_ARGS, args.gateway))
 child.expect(PROMPT)
-child.sendline('opkg update')
-child.expect(PROMPT)
-child.sendline('opkg install kmod-usb-core kmod-usb-ohci kmod-usb-uhci kmod-usb2 usbutils kmod-usb-storage kmod-fs-ext4 kmod-usb-storage-extra block-mount e2fsprogs')
-child.expect(PROMPT)
+install_packages(child, USB_MODULES)
+
+log("Checking %s..." % (DEV_PATH))
 child.sendline('ls -al %s' % (DEV_PATH))
 try :
   child.expect(DEV_PATH)
   child.expect(PROMPT)
+  log("USB disk detected.")
 except :
   log("No USB disk detected. Aborting...")
   child.kill(0)
   exit(0)
+
 
 log("Checking extroot...")
 child.sendline('df -h')
@@ -146,9 +172,12 @@ match = child.expect([DEV_PATH, PROMPT])
 if match == 0 :
   log("Already using extroot.")
 else :
+  log("No extroot detected. Creating...")
   log("Making ext4 file system...")
   child.sendline('mkfs.ext4 %s' % (DEV_PATH))
-  child.expect(PROMPT)
+  child.expect('Creating journal')
+  child.expect('Writing superblocks')
+  child.expect(PROMPT, timeout=60)
   child.sendline('mkdir -p /mnt/usb')
   child.expect(PROMPT)
 
@@ -156,11 +185,12 @@ else :
   child.sendline('mount -t ext4 %s /mnt/usb' % (DEV_PATH))
   child.expect(PROMPT)
   child.sendline('tar -C /overlay -cvf - . | tar -C /mnt/usb -xf -')
-  child.expect(PROMPT)
+  child.expect(PROMPT, timeout=60)
 
   child.sendline('reboot')
   child.kill(0)
 
+  log("Waiting for router reboot...")
   child = pexpect.spawn('ping %s' % (args.gateway))
   try :
     child.expect('ttl', timeout=30)
@@ -175,16 +205,15 @@ child.sendline('df -h')
 try :
   child.expect(DEV_PATH)
   child.expect(PROMPT)
+  log("extroot succeed.")
 except :
   log("No USB partition detected, extroot failed.")
   exit(0)
 
-log("Installing packages...")
-child.sendline('opkg update')
-child.expect(PROMPT)
-child.sendline('opkg install shadow-useradd shadow-groupadd shadow-usermod sudo sed python vim bash coreutils-* shadow-userdel')
-child.expect(PROMPT)
 
+
+log("Installing packages...")
+install_packages(child, EXTRA_MODULES)
 
 
 
@@ -200,7 +229,6 @@ else :
   child.expect(PROMPT)
   child.sendline('sed -i -e \'s@^#\%sudo.*$@\%sudo ALL=(ALL) NOPASSWD: ALL/\' /etc/sudoers')
   child.expect(PROMPT)
-
 
 
 
